e6f316ef29a7ebf21829662046ad469408bd6d3e
Michael Beukman <michael.beukman@gmail.com>
2022-12-20 16:02:38 +0200

    some more plots per lang

--- scripts/plot_graphs.py
+++ scripts/plot_graphs.py
@@ -19,6 +19,7 @@ MODES = ['global_cap_labels', 'global_cap_sentences', 'global_swap_labels',
 
 
 def savefig(name, pad=0):
+    plt.tight_layout()
     if '/' in name:
         path = '/'.join(name.split('/')[:-1])
         os.makedirs(path, exist_ok=True)
@@ -28,7 +29,7 @@ def savefig(name, pad=0):
     name = name.split(".png")[0] + ".pdf"
     plt.savefig(name, bbox_inches='tight', pad_inches=pad, dpi=200)
 
-def main():
+def main(lang_to_use=None):
     model = 'afriberta'
     mode = 'global_cap_labels'
     # for nums in range(1, 11):
@@ -41,6 +42,7 @@ def main():
         'f1':       [],
     }
     SHOULD_READ = True
+    langs_to_use = LANGS  #[l for l in LANGS if l == lang_to_use or lang_to_use is None]
     for mode in MODES:
         if not SHOULD_READ: break
         for model in MODELS:
@@ -53,7 +55,7 @@ def main():
                 perc = nums / 10
                 if mode.startswith('local'): perc = nums
                 if mode == 'original': perc = 1
-                for lang in LANGS:
+                for lang in langs_to_use:
                     for seed in range(1, 4):
                         dir = f"{ROOT}/{perc}/{lang}/{seed}/test_results.txt"
                         if mode.startswith('local_swap_labels') and perc == 0:
@@ -83,8 +85,8 @@ def main():
         df.to_csv("analysis/main_results.csv")
     # exit()
     df = pd.read_csv("analysis/main_results.csv")
-    
-    for lang in LANGS:
+    old = copy.deepcopy(df)
+    for lang in langs_to_use:
         for model in MODELS:
             
             temp = df.loc[df['lang'] == lang]
@@ -94,56 +96,51 @@ def main():
             assert len(temp) == 1
             opt = temp['f1'].item()
             df.loc[np.logical_and(df['lang'] == lang, df['model'] == model), 'good'] = df.loc[np.logical_and(df['lang'] == lang, df['model'] == model), 'f1'] / opt
-            # T['good'] = T['f1'] / opt
-            # good['good'] = good['f1'] / opt
-    # df = good
-        # df['good']
-    
-    # df = df[df['model'] == 'afriberta']
-    # df = df[df['mode'] == 'global_cap_labels']
-    # df = df[df['mode'] == 'global_cap_sentences']
-    
     old = df
-    for mode in MODES[:-1]:
+    def single_plot(mode, AX, lang=None):
         df = copy.deepcopy(old)
         df = df[df['mode'] == mode]
-        # if mode == 'local_swap_labels':
-            # df['num'] = 11 - df['num']
+        
+        if lang is not None: 
+            df = df[df['lang'] == lang]
+            AX.set_title(lang)
+        
         df = df.groupby(['mode', 'num', 'model', 'lang'], as_index=False).mean()
-        print(len(df))
-        print(df)             
-        # plt.plot(df['num'], df['good'])
-        sns.lineplot(df, x='num', y='good', hue='model', errorbar='sd')
-        # sns.lineplot(df, x='num', y='good', hue='lang')
-        # plt.plot(df['num'], df['num'])
+        sns.lineplot(df, x='num', y='good', hue='model', errorbar='sd', ax=AX)
         x = np.unique(df['num'])
+        # AX = plt.gca()
+        
         if mode == 'local_swap_labels':
-            plt.plot(x, 1 - np.arange(0, 11)/10)
+            AX.plot(x, 1 - np.arange(0, 11)/10)
         elif mode == 'global_cap_sentences':
-            plt.plot(x, np.array([0.1, 0.5] + list(np.arange(1, 11)))/10)
-        # elif mode.startswith('local'):
-            # plt.plot(x, np.arange(0, 11)/10)
+            AX.plot(x, np.array([0.1, 0.5] + list(np.arange(1, 11)))/10)
         else:
-            plt.plot(x, np.arange(1, 11)/10, label='Linear Relationship')
-        plt.xlabel("Level of Quality")
+            AX.plot(x, np.arange(1, 11)/10, label='Linear Relationship')
+        AX.set_xlabel("Level of Quality")
         if mode == 'global_cap_sentences':
-            plt.xlabel("Fraction of Sentences Kept")
+            AX.set_xlabel("Fraction of Sentences Kept")
         if mode == 'global_cap_labels':
-            plt.xlabel("Fraction of Labels Kept")
+            AX.set_xlabel("Fraction of Labels Kept")
         if mode == 'global_swapped_labels':
-            plt.xlabel("Fraction of Labels Swapped")
+            AX.set_xlabel("Fraction of Labels Swapped")
         if mode == 'local_cap_labels':
-            plt.xlabel("Maximum number of labels kept per sentence")
+            AX.set_xlabel("Maximum number of labels kept per sentence")
         if mode == 'local_swap_labels':
-            plt.xlabel("Maximum number of labels swapped per sentence")
+            AX.set_xlabel("Maximum number of labels swapped per sentence")
         
-        plt.ylabel("Fraction of F1 when using original dataset")
+        AX.set_ylabel("Fraction of F1 when using original dataset")
         
+    for mode in MODES[:-1]:
+        single_plot(mode, plt.gca())
         savefig(f'analysis/plots/corruption/{mode}.png')
         plt.close()
-        # plt.show()
-        
-    pass
+    if lang_to_use is not None:
+        for mode in MODES[:-1]:
+            fig, axs = plt.subplots(4, 3, figsize=(18, 24))
+            for ax, lang in zip(axs.ravel(), LANGS):
+                single_plot(mode, ax, lang)
+            savefig(f'analysis/plots/corruption/langs/all_{mode}.png')
+            plt.close()
 
 
 def plot_dataset_stats():
@@ -206,6 +203,6 @@ def plot_entity_frequency():
     # plt.plot(L['Nums'], L['swa'])
     plt.show()
 if __name__ == '__main__':
-    main()
+    main(True)
     # plot_dataset_stats()
     # plot_entity_frequency()
