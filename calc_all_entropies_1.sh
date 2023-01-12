9588fccbeaccfa4849ee5e014a7585a9346e354e
Arnol Fokam <arnolfokam23@gmail.com>
2023-01-13 00:34:23 +0200

    code

--- calc_all_entropies_1.sh
+++ calc_all_entropies_1.sh
@@ -1,8 +1,8 @@
 count=0
 
-for model in xlmr; do
-	for corruption in global_cap_labels global_cap_sentences global_cap_sentences_seed1 global_cap_sentences_seed2 global_swap_labels local_cap_labels local_swap_labels; do
-		for param in 0.1 0.01 0.2 0.3 0.4 0.5 0.05 0.6 0.7 0.8 0.9 1.0 1 2 3 4 5 6 7 8 9 10; do	
+for model in afro_xlmr; do
+	for corruption in local_swap_labels_like_cap; do
+		for param in  1 2 3 4 5 6 7 8 9 10; do	
 			for lang in amh conll_2003_en hau ibo kin lug luo pcm swa wol yor; do
 				for seed in 1 2 3; do
 					./run.sh scripts/get_entropies.py --model_type $model --corruption_name $corruption --param $param --language $lang --number_of_predictions 50 --seed $seed &
