cea3baa7ef29801aced79f0a523fef8f238ddb0b
Michael Beukman <michael.beukman@gmail.com>
2023-01-14 19:47:19 +0200

    cleaned entropy

--- scripts/analyse_entropies.py
+++ scripts/analyse_entropies.py
@@ -6,14 +6,10 @@ import numpy as np
 from scripts.plot_graphs import CLEAN_MODES, savefig, clean_model
 
 def get_vals(folder):
-    # "entropies/xlmr/global_swap_labels/0.5/swa/3/"
     A = folder.split("/")
     original_data = os.path.join(*(A[:2] + ['global_cap_sentences', '1.0'] + A[4:]))
-    og_ent = np.load(os.path.join(original_data, 'entropies.npz'))['arr_0']
-    og_labs = np.load(os.path.join(original_data, 'labels.npz'))['arr_0']
     ent = np.load(os.path.join(folder, 'entropies.npz'))['arr_0']
     labs = np.load(os.path.join(folder, 'labels.npz'))['arr_0']
-    assert np.all(labs == og_labs)
     with open(os.path.join(folder, 'labels.p'), 'rb') as f:
         lab2 = pickle.load(f)
 
@@ -25,89 +21,36 @@ def get_vals(folder):
             if example_labels[j] != -100:
                 break
         
-        if 0:
-            # Mike this is old, but correct
-            example_labels = example_labels[:j + 1]
-            new = []
-            curr = -100
-            for i in example_labels:
-                if i == -100:
-                    new.append(curr)
-                else:
-                    curr = i
-                    new.append(i)
-            new = new[1:]
-            example = example[1:]
-        else:
-            example_labels = example_labels[:j + 1]
-            new = []
-            new_ent = []
-            og_new_ent = []
-            curr = -100
-            for idx, (i, myent) in enumerate(zip(example_labels, example)):
-                if i == -100:
-                    continue
-                else:
-                    curr = i
-                    new.append(i)
-                    new_ent.append(myent)
-                    og_new_ent.append(og_ent[index][idx])
-            # new = new[1:]
-            example = new_ent
+        example_labels = example_labels[:j + 1]
+        new = []
+        new_ent = []
+        # Ignoring the -100 tokens
+        for idx, (i, myent) in enumerate(zip(example_labels, example)):
+            if i == -100:
+                continue
+            else:
+                new.append(i)
+                new_ent.append(myent)
+        example = new_ent
         
         old = new
         new = [n if n == 0 else 1 for n in new]
         
         x_0 = []
         x_1 = []
-        # for n, ex in zip(new, example):
-        #     if n == 0:   x_0.append(ex)
-        #     elif n == 1: x_1.append(ex)
-        #     else: assert False
 
-        # good_ents.extend([np.mean(x_0) if len(x_0) else 0, np.mean(x_1) if len(x_1) else 0])
-        # good_labs.extend([0, 1])
-
-        
-        # a, b = example[:j + 1], og_ent[index][:j + 1]
-        # a, b = np.array(example), np.array(og_new_ent) #og_ent[index]
-        a, b = (example), (og_new_ent) #og_ent[index]
-        # print(a[:2], b[:2], (a / b)[:2])
-        good_ents.extend(a)# / b)
+        good_ents.extend(example)
         good_labs.extend(new)
         good_labs2.extend(old)
-        # good_labs.extend(example_labels[:j + 1])
-        # print("NEW", new)
-        # print(new)
-        # exit()
     x_0 = []
     x_1 = []
-    if 1:
-        for n, ex in zip(good_labs, good_ents):
-            if n == 0:   x_0.append(ex)
-            elif n == 1: x_1.append(ex)
-            else: assert False
-        if 0: #mike hacks
-            good_ents = np.array(good_ents)
-            for label in np.unique(good_labs2):
-                break
-                a = good_ents[np.array(good_labs2) == label]
-                print(f'{label}, {np.min(a):<25}, {np.max(a):<25}, {np.mean(a):<25}, {np.std(a):<25}')
-            # exit()
-            print(min(x_0), max(x_0), np.mean(x_0), np.std(x_0))
-            print(min(x_1), max(x_1), np.mean(x_1), np.std(x_1))
-            exit()
-        if   0:
-            good_ents = x_1
-            good_labs = [1] * len(x_1)
-        elif 1:
-            good_ents = [np.mean(x_0) if len(x_0) else 0, np.mean(x_1) if len(x_1) else 0][1:]
-            good_labs = [0, 1][1:]
-        else:
-            good_ents = [np.mean(x_0) if len(x_0) else 0, np.mean(x_1) if len(x_1) else 0][:1]
-            good_labs = [0, 1][:1]
+    for n, ex in zip(good_labs, good_ents):
+        if n == 0:   x_0.append(ex)
+        elif n == 1: x_1.append(ex)
+        else: assert False
+    good_ents = [np.mean(x_0) if len(x_0) else 0, np.mean(x_1) if len(x_1) else 0][1:]
+    good_labs = [0, 1][1:]
     return good_ents, good_labs, lab2
-    return ent, labs, lab2
 
 
 MODELS = ['xlmr', 'afriberta', 'afro_xlmr', 'mbert']
@@ -151,27 +94,17 @@ def main(MODEL='afro_xlmr', CORRUPTION='global_swap_labels'):
                 else:
                     if XXXX[I] is None:
                         XXXX[I] = v
-                    print("LEN V", len(v))
                     vals.append(m:= np.mean(v))
                     s = 0
                     mi.append(np.min(v))
                     ma.append(np.max(v))
             ax.plot(x, vals, label=clean_model(MODEL))
-            PPP = ogv[2]
-            PPP = ["O", "Entities"]
-            LLL = X[I]
-            print(PPP)
-            print(LLL)
-            # ax.set_title(str((PPP[LLL]) if LLL >= 0 else LLL) + " -- " + str(idx.sum()))
             ax.fill_between(x, mi, ma, alpha=0.1)
             ax.set_ylabel("Entropy")
-            # ax.set_xlabel("Param")
             ax.set_xlabel(mytest(CORRUPTION))
             ax.set_ylim(0, 1.3)
             ax.set_title(CLEAN_MODES[CORRUPTION])
-            # ax.show()
     plt.legend()
-    # plt.suptitle(CORRUPTION)
     savefig(f'analysis/plots/entropies/ALL_{CORRUPTION}.png')
     
     plt.close()
@@ -191,6 +124,5 @@ def mytest(mode):
 
 
 if __name__ == '__main__':
-    for M in ['xlmr', 'afriberta', 'afro_xlmr', 'mbert'][:1]: 
-        for C in ['global_swap_labels', 'global_cap_labels', 'global_cap_sentences', 'local_cap_labels']:
-            main(M, C)
+    for C in ['global_swap_labels', 'global_cap_labels', 'global_cap_sentences', 'local_cap_labels']:
+        main(None, C)
