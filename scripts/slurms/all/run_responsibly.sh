ffa7f25c5212fed5110230b5b4cca0f7ca1fcca2
Arnol Fokam <arnolfokam23@gmail.com>
2023-05-04 13:27:40 +0200

    code

--- scripts/slurms/all/run_responsibly.sh
+++ scripts/slurms/all/run_responsibly.sh
@@ -2,7 +2,7 @@ touch datastore2
 # v0150_1-a v0150_1-b v0150_1-c v0150_1-d v0150_1-e v0150_1-f v0150_1-g v0150_1-h v0150_1-i v0150_1-j v0150_1-k v0150_1-l v0151_4-a v0151_4-b v0151_4-c v0151_4-d v0148-a v0148-b v0148-c 
 for i in `cat all_exps`; do 
     NUM_JOBS=`squeue | grep mfokam | grep bigbatch | wc -l`
-    N=15
+    N=20
     if ! grep -q $i datastore2; then
         # echo "Not found $i. Possibly Running"
         if [ "$NUM_JOBS" -lt "$N" ]; then
